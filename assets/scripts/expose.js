89aa0437b1caa0b298a00cdb8000c2559f4a89d9
mishkajethwani <mjethwani@ucsd.edu>
2024-05-05 21:08:00 -0700

    volume icons

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,6 +5,8 @@ function init() {
   const horn_select = document.getElementById('horn-select');
   const button = document.querySelector('button');
   const audio = document.querySelector('audio');
+  
+  
 
   horn_select.addEventListener('change', () => {
     document.querySelector('img').src = `assets/images/${horn_select.value}.svg`;
@@ -20,5 +22,23 @@ function init() {
         emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
      })
     }
+  });
+
+  const volume_button_select = document.getElementById('volume');
+  const volume_img = document.querySelector('#volume-controls img');
+  volume_button_select.addEventListener ('input', () => {
+    if (volume_button_select.value == 0) {
+      volume_img.src = 'assets/icons/volume-level-0.svg';
+    } 
+    else if(volume_button_select.value < 33){
+      volume_img.src = 'assets/icons/volume-level-1.svg';
+    } 
+    else if(volume_button_select.value < 67){
+      volume_img.src = 'assets/icons/volume-level-2.svg';
+    }
+    else{
+      volume_img.src = 'assets/icons/volume-level-3.svg';
+    }
+
   });
 }
